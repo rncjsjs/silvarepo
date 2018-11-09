2653e66b8cbe31276e5af2da5b3a72e676df470d
silva <silva@supernova11.com>
2018-11-09 15:26:15 +0900

    재등록

---
+++ shooting/Assets/Editor/Autobuilder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Autobuilder : MonoBehaviour {
+
+	public static void BuildGame () {
+
+        var scenes = new string[] { "Assets/3D shooting.unity" };
+        var flags = BuildOptions.Development;
+        Debug.Log("start");
+        BuildPipeline.BuildPlayer(scenes, "test.apk", BuildTarget.Android, flags);
+        Debug.Log("end");
+
+    }
+
+}
---
+++ shooting/Assets/Script/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float Speed = 0.10f;
+    public GameObject bullet;
+        
+	void Update () {
+
+        float moveAmnt = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+        float moveAmnt2 = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+        Vector3 moveVector = Vector3.right * moveAmnt + Vector3.up * moveAmnt2;
+        transform.Translate(moveVector);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<AudioSource>().Play();
+            Instantiate(bullet, transform.position, transform.rotation);
+        }
+	}
+}
